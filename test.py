495d0dd18b7fb6fa87193a2e0ffa9de0b9b20f6a
Hilari <hilari.gomez@agilogy.com>
2025-06-03 17:13:43 +0200

    Added docstring for class and functions in test.py

--- test.py
+++ test.py
@@ -1,20 +1,24 @@
-"""Tests"""
+"""Tests CI/CD"""
 
 import unittest
 import transform
 
 
 class TestStringMethods(unittest.TestCase):
+    """Test Class"""
 
     def test_is_upper(self):
+        """Returns true if a string is upper case"""
         sting = transform.to_upper_case("hello")
         self.assertEqual(sting, "HELLO")
 
     def test_is_lower(self):
+        """Returns true if a string is lower case"""
         sting = transform.to_lower_case("HELLO")
         self.assertEqual(sting, "hello")
 
     def test_is_capitalize(self):
+        """Returns true if a string starts with a capital letter"""
         sting = transform.to_capitalize("HELLO")
         self.assertEqual(sting, "Hello")
 
